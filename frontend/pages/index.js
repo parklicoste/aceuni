15665069e03324f2ca17c830e7a5fb258dc8009b
parklicoste <khushalkumar@cmail.carleton.ca>
2022-03-11 12:08:28 -0500

    Initialized the structure, Login page structure

---
+++ frontend/pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Login from './components/Login';
+import styles from 'carbon-components/css/carbon-components.min.css'
+// import styles from '../node_modules/carbon-components-react/umd/carbon-components-react.min.js'
+
+export default function Home() {
+  return (
+    <Login />
+  )
+}
