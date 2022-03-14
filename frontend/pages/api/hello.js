7631f1c3ea70407f36551a2889c4356ea6643078
parklicoste <khushalkumar@cmail.carleton.ca>
2022-03-13 20:59:05 -0400

    added password encryption file

--- frontend/pages/api/hello.js
+++ frontend/pages/api/hello.js
@@ -1,5 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+import { sendPasswordHash } from "./encryption"
 export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(
+    { username: 'admin@admin.com',
+    password: sendPasswordHash('admin')
+ })
 }
+// words": [
+//   -1939245339,
+//   -1254030315,
+//   -1108801347,
+//   1307448799,
+//   -1318606392,
+//   1945914296,
+//   -1474334934,
+//   -1270306536
+// ],
