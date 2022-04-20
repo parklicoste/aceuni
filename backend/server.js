74efab40220338332981344acd5be7869f83bcfd
parklicoste <khushalkumar@cmail.carleton.ca>
2022-04-19 22:25:52 -0400

    added mongo URI

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,11 @@ app.use(express.json());
 const PORT = process.env.PORT || 8080;
 
 mongoose
- .connect('mongodb+srv://aceuni:Carleton4905@khushalkumar.17pb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority') //Replace
+ .connect('mongodb+srv://aceuni:Carleton4905@khushalkumar.172pb.mongodb.net/test') 
+//  Replace
+//  mongodb+srv://aceuni:<password>@khushalkumar.172pb.mongodb.net/test
+// mongodb+srv://aceuni:Carleton4905@khushalkumar.17pb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+// https://data.mongodb-api.com/app/data-dbxkx/endpoint/data/beta?
  .then(()=>console.log("DBConnection successful!"))
  .catch((err)=>{
     console.log(err);
